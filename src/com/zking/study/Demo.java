425685dfa325c94887578736261c08c8f11595a6
Administrator <yxf_magical@qq.com>
2018-01-26 12:57:32 +0800

    idea测试github

---
+++ src/com/zking/study/Demo.java
@@ -0,0 +1,17 @@
+package com.zking.study;
+
+import java.io.Serializable;
+
+/**
+ * @PROJECT_NAME test_github
+ * @Author: Administrator
+ * @Description:
+ * @Date: Created in 2018/1/26 12:51
+ */
+public class Demo implements Serializable {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 1;
+        System.out.println(a + b + a + b);
+    }
+}
